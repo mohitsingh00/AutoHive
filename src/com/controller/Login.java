4ac21898e562110aa1e00e82da6859861080b26b
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-08 18:46:18 +0530

    Remove AddNewAdmin & AddNewClient class

--- src/com/controller/Login.java
+++ src/com/controller/Login.java
@@ -1,26 +1,40 @@
 package com.controller;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.model.Admin;
+import com.model.Client;
 import com.model.Database;
-import com.mysql.cj.protocol.Resultset;
+import com.model.User;
 
 public class Login {
 
-	public Login(Database database, Scanner sc)
-	{
-		System.out.println("Enter Email: ");
+	public static void main(String[] args) {
+		Database database = new Database();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to Car Rental System");
+		System.out.println("Enter Email: \n(-1) to create new account");
 		String email = sc.next();
+		if(email.equals("-1"))
+		{
+			new AddNewAccount(0).operation(database, sc, null);
+			return;
+		}
 		System.out.println("Enter Password: ");
 		String password = sc.next();
 		
-		try
+		ArrayList<User> users = new ArrayList<>();
+		
+		try 
 		{
 			String select = "SELECT * FROM `users`";
 			ResultSet rs = database.getStatement().executeQuery(select);
 			while(rs.next())
 			{
+				User user;
 				int ID = rs.getInt("ID");
 				String firstName = rs.getString("FirstName");
 				String lastName = rs.getString("LastName");
@@ -28,11 +42,40 @@ public class Login {
 				String phoneNumber = rs.getString("PhoneNumber");
 				String pass = rs.getString("Password");
 				int type = rs.getInt("Type");
+				switch(type)
+				{
+				case 0:
+					user = new Client();
+					break;
+				case 1:
+					user = new Admin();
+					break;
+				default:
+					user = new Client();
+					break;
+				}
+				user.setID(ID);
+				user.setFirstName(firstName);
+				user.setLastName(lastName);
+				user.setEmail(em);
+				user.setPhoneNum(phoneNumber);
+				user.setPassword(pass);
+				users.add(user);
+				
 			}
 		}
 		catch (Exception e)
 		{
-			
+			e.printStackTrace();
+		}
+		
+		for(User u : users)
+		{
+			if(u.getEmail().equals(email) && u.getPassword().equals(password))
+			{
+				System.out.println("Welcome "+u.getFirstName()+"!");
+				u.showList(database, sc);
+			}
 		}
 	}
 }
--- src/com/model/Admin.java
+++ src/com/model/Admin.java
@@ -1,15 +1,14 @@
 package com.model;
 
 import java.util.Scanner;
-
-import com.controller.AddNewAdmin;
+import com.controller.AddNewAccount;
 
 public class Admin extends User {
 	
-	private Operation[] operations = new Operation[] {new AddNewAdmin()};
+	private Operation[] operations = new Operation[] {new AddNewAccount(1)};
 	public Admin() {
 		super();
-	}
+	} 
 
 	@Override
 	public void showList(Database database, Scanner sc)
@@ -23,10 +22,6 @@ public class Admin extends User {
 		System.out.println("7. Quit\n");
 		
 		int i = sc.nextInt();
-		operations[i].operation(database, sc, this);
+		operations[0].operation(database, sc, this);
 	}
-
-	
-
-	
 }
