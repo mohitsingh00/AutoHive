2a557b71325bdbce56f85b8eee5eaa98d27cb5ad
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-09 19:05:16 +0530

    Add Show User Rent Functionality

---
+++ src/com/controller/ShowUserRents.java
@@ -0,0 +1,88 @@
+package com.controller;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Scanner;
+import com.model.Car;
+import com.model.Client;
+import com.model.Database;
+import com.model.Operation;
+import com.model.Rent;
+import com.model.User;
+
+public class ShowUserRents implements Operation {
+	
+	private int userID;
+	
+	public ShowUserRents(int userID) {
+		this.userID = userID;
+	}
+
+	@Override
+	public void operation(Database database, Scanner sc, User user) 
+	{
+		ArrayList<Rent> rents = new ArrayList<>();
+		ArrayList<Integer> carIDs = new ArrayList<>();
+		try
+		{
+			String select = "SELECT * FROM `rents` WHERE `User` = '"+userID+"';";
+			ResultSet rs = database.getStatement().executeQuery(select);
+			while(rs.next())
+			{
+				Rent rent = new Rent();
+				rent.setID(rs.getInt("ID"));
+				carIDs.add(rs.getInt("Car"));
+				rent.setDateTime(rs.getString("DateTime"));
+				rent.setHours(rs.getInt("Hours"));
+				rent.setTotal(rs.getDouble("Total"));
+				rent.setStatus(rs.getInt("Status"));
+				rents.add(rent);
+			}
+			
+			String selectUser = "SELECT * FROM `users` WHERE `ID` = '"+userID+"';";
+			ResultSet rs2 = database.getStatement().executeQuery(selectUser);
+			rs2.next();
+			User u = new Client();
+			u.setID(rs.getInt("ID"));
+			u.setFirstName(rs2.getString("FirstName"));
+			u.setLastName(rs2.getString("LastName"));
+			u.setEmail(rs2.getString("Email"));
+			u.setPhoneNum(rs2.getString("PhoneNumber"));
+			u.setPassword(rs2.getString("Password"));
+			for(int j = 0; j < rents.size(); j++)
+			{
+				Rent r = rents.get(j);
+				r.setUser(u);
+				ResultSet rs3 = database.getStatement()
+				.executeQuery("SELECT * FROM `cars` WHERE `ID` = '"+carIDs.get(j)+"';");
+				
+				rs3.next();
+				Car car = new Car();
+				car.setID(rs3.getInt("ID"));
+				car.setBrand(rs3.getString("Brand"));
+				car.setModel(rs3.getString("Model"));
+				car.setColor(rs3.getString("Color"));
+				car.setYear(rs3.getInt("Year"));
+				car.setPrice(rs3.getDouble("Price"));
+				car.setAvailable(rs3.getInt("Available"));
+				r.setCar(car);
+				
+				System.out.println("ID: "+r.getID());
+				System.out.println("Name: "+r.getUser().getFirstName()+" "+r.getUser().getLastName());
+				System.out.println("Email: "+r.getUser().getEmail());
+				System.out.println("Phone Number: "+r.getUser().getPhoneNum());
+				System.out.println("Car ID: "+r.getCar().getID());
+				System.out.println("Car: "+r.getCar().getBrand()+" "+r.getCar().getModel()+" "+r.getCar().getColor());
+				System.out.println("Date Time: "+r.getDateTime());
+				System.out.println("Hours: "+r.getHours());
+				System.out.println("Total: "+r.getTotal());
+				System.out.println("Status: "+r.getStatusToString());
+				System.out.println("--------------------------------------");
+			}
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
