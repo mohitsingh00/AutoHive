f2911b1b26dba6689d5f3545a87692ce2f7b475e
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-09 12:27:01 +0530

    Created RentCar class

---
+++ src/com/controller/RentCar.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import java.util.Scanner;
+
+import com.model.Database;
+import com.model.Operation;
+import com.model.User;
+
+public class RentCar implements Operation {
+
+	@Override
+	public void operation(Database database, Scanner sc, User user) 
+	{
+		
+		
+	}
+
+}
