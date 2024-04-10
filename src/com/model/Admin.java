6d5b5ba2d765ef44dd375915875a6f370d5e3236
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-10 17:18:49 +0530

    Bug Fixes

--- src/com/model/Admin.java
+++ src/com/model/Admin.java
@@ -3,16 +3,31 @@ package com.model;
 import java.util.Scanner;
 import com.controller.AddNewAccount;
 import com.controller.AddNewCar;
+import com.controller.ChangePassword;
 import com.controller.DeleteCar;
+import com.controller.EditUserData;
+import com.controller.Quit;
 import com.controller.ShowAllRents;
+import com.controller.ShowSpecUserRents;
 import com.controller.UpdateCar;
 import com.controller.ViewCars;
 
 public class Admin extends User {
 	
-	private Operation[] operations = new Operation[] {new AddNewCar(), new ViewCars(),
-			new UpdateCar(), new DeleteCar(), new AddNewAccount(1), new ShowAllRents()};
-	public Admin() {
+	private Operation[] operations = new Operation[] {new AddNewCar(), 
+			 										  new ViewCars(),
+			 										  new UpdateCar(), 
+			 										  new DeleteCar(), 
+			 										  new AddNewAccount(1),
+			 										  new ShowAllRents(),
+			 										  new ShowSpecUserRents(),
+			 										  new EditUserData(),
+			 										  new ChangePassword(),
+			 										  new Quit()
+			 										  };
+	
+	public Admin()
+	{
 		super();
 	} 
 
@@ -25,13 +40,18 @@ public class Admin extends User {
 		System.out.println("4. Delete Car");
 		System.out.println("5. Add New Admin");
 		System.out.println("6. Show Rents");
-	//	System.out.println("7. Show User's Rents");
+		System.out.println("7. Show User's Rents");
 		System.out.println("8. Edit My Profile");
 		System.out.println("9. Change Password");
-		System.out.println("10. Quit\n");
+		System.out.println("10. Quit\n"); 
 		
 		int i = sc.nextInt();
+		if(i<1 || i>10)
+		{
+			showList(database, sc);
+			return;
+		}
 		operations[i-1].operation(database, sc, this);
-		showList(database,sc);
+		if(i != 10) showList(database, sc);
 	}
 }
