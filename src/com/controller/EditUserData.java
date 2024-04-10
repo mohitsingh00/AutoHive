e706ef25fbdbf3530f2c1cb0eba89c5340a06480
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-10 11:52:12 +0530

    Add edit user data class

---
+++ src/com/controller/EditUserData.java
@@ -0,0 +1,17 @@
+package com.controller;
+import java.util.Scanner;
+
+import com.model.Database;
+import com.model.Operation;
+import com.model.User;
+
+public class EditUserData implements Operation{
+
+	@Override
+	public void operation(Database database, Scanner sc, User user) 
+	{
+		
+		
+	}
+
+}
